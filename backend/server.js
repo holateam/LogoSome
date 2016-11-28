7529708c7a7c0a70aa715dac4cd2b44dbb009bb3
Alex Belokon <info.tehnokot@gmail.com>
2016-11-28 19:15:24 +0200

    backend - added server.js and confg.json

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,15 @@
 /**
  * Created by lex on 28.11.16.
  */
+const config = require('./config.json');
+const express = require('express');
+const app = express();
+const server =  require('http').createServer(app);
 
+
+
+
+
+server.listen(config.port, () => {
+    console.log(`Web Server running on port ${config.port}`);
+});
