de2b7e9de32a578fb57b0aac58a72e1add606b91
krusso <kstn.russo@gmail.com>
2016-12-02 18:41:04 +0200

    firts searcher commit

---
+++ log-searcher-process/log-searcher-process.js
@@ -0,0 +1,55 @@
+// searchInFiles(5000, filesGetter, undefined, "*", 0, 100, f)
+// function searchInFiles(heartbeatInterval, filenameGetterFunc,
+//                        startFromLogfile?null, filter, skipLinesCount, limit, updatesHandler) {
+//     //... X:
+//     function gotoNextFile() {
+//         filename = filenameGetterFunc(filename);
+//         if (filename == undefined)
+//             return updatesHandler(true, ...); // end of search
+//
+//         searchInFile(heartbeatInterval, filename, filter, skipLinesCount, limit~,
+//             function(fileFinished, loglines, lastlineindex) {
+//                 updatesHandler(false, filename, loglines, lastlineindex);
+//                 limit -= loglines.length;
+//                 if (fileFinished) {
+//                     gotoNextFile();
+//                 }
+//             })
+//     }
+//
+//
+//     gotoNextFile();
+//     //...
+//     return {
+//         stop: function() {
+//             //...
+//         }
+//     }
+// }
+
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+options = {
+    port: 4000
+};
+
+let runSearcherProcess = (port) => {
+    http.listen(port, () => {
+        console.log(`server is running on port ${port}`);
+    });
+    app.use(express.static(__dirname + '/'));
+
+    io.on('connection', (socket) => {
+        console.log('connection have been established');
+
+        socket.on('getLogs', (paramsJson) => {
+            createSearcherInstanceWithParamsForEachStreamId(paramsJson);
+        });
+    });
+};
+
+runSearcherProcess(options.port);
+
