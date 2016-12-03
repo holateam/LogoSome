7397debb6d0bf7f30dd55f6ce83077b8e3a49643
Alex Belokon <info.tehnokot@gmail.com>
2016-12-03 22:05:40 +0200

    add func receiver and fix db func

---
+++ backend/routes/service.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const db_query = require('../modules/db_query');
+
+let standardRes = (err, resp) => {
+    return err ? {
+        success: !err,
+        msg: resp
+    } : {
+        success: !err,
+        data: resp
+    };
+};
+
+router.get('/api/v1/getUsers', (req, res, next) => {
+    db_query.getUsers().then((result) => {
+        console.log(JSON.stringify(result));
+       res.json(200, standardRes(result.err, result.data));
+    }).catch((result) => {
+        res.json(403, standardRes(result.err, result.data));
+    });
+
+});
+
+// router.route('/api/v1/cookie-session').post((req, res, next) => {
+//     db_query.cookieSession(req.body.cookie).then((result) => {
+//         res.json(200, standardRes(result.err, result.data));
+//     }).catch((result) => {
+//         res.json(500, standardRes(result.err, result.data));
+//     });
+// });
+
+module.exports = router;
